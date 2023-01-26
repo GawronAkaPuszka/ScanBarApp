bd8b842c8a8b2ec31531e9d8d424a3391ee94cf4
GawronAkaPuszka <piotr.gawronski203@gmail.com>
2023-01-27 00:01:41 +0100

    Added WebView functionality.

--- app/src/main/java/com/example/test4/DetailsActivity.java
+++ app/src/main/java/com/example/test4/DetailsActivity.java
@@ -3,20 +3,26 @@ package com.example.test4;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 public class DetailsActivity extends AppCompatActivity {
 
-    TextInputEditText txtInput;
+    TextView txtInput;
     Button btFinishActivity;
+    WebView myWebView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
         btFinishActivity = findViewById(R.id.bt_finish_details_activity);
+        myWebView = findViewById(R.id.web_view);
         btFinishActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,7 +34,9 @@ public class DetailsActivity extends AppCompatActivity {
             String value = extras.getString("ID_CODE");
             txtInput = findViewById(R.id.txtInput);
             txtInput.setText(value);
-            //The key argument here must match that used in the other activity
+            myWebView.loadUrl("https://www.google.com/search?q=" + value);
+            WebSettings webSettings = myWebView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
         }
     }
 
